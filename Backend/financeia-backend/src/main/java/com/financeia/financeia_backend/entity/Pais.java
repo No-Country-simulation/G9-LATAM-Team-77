c2621cf5479fd5b1d4fb66de3e6f670177b6ea82
Irwing Jonathan Ramirez Paz <ijonathansn00@gmail.com>
2026-08-09 22:18:16 -0600

    fix: configurar H2 y completar entidades JPA

--- Backend/financeia-backend/src/main/java/com/financeia/financeia_backend/entity/Pais.java
+++ Backend/financeia-backend/src/main/java/com/financeia/financeia_backend/entity/Pais.java
@@ -1,4 +1,25 @@
 package com.financeia.financeia_backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pais {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Pais() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
